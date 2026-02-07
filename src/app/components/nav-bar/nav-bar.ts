e98d2bb449646872a7de83d5e1a8514954128ed0
ramphy <ramphy_an@outlook.com>
2026-02-07 15:30:02 +0100

    feat: nav bar

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { NavBarItem } from '../../types/nav-bar-item';
+import { NAV_BAR_ROUTES } from '../../app.routes';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
 	selector: 'nav-bar',
-	imports: [],
+	imports: [CommonModule, RouterLink, RouterLinkActive],
 	templateUrl: './nav-bar.html',
 	styleUrl: './nav-bar.css',
 })
-export class NavBar {}
+export class NavBar {
+	navOptions: readonly NavBarItem[] = NAV_BAR_ROUTES;
+}
