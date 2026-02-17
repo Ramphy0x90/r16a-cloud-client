461298b362a08d1d1b4fb449359e501cdc8487a4
ramphy <ramphy_an@outlook.com>
2026-02-17 21:26:41 +0100

    min: style updates

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -3,10 +3,11 @@ import { NavBarItem } from '../../types/nav-bar-item';
 import { NAV_BAR_ROUTES } from '../../app.routes';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IdentityLogo } from '../identity-logo/identity-logo';
 
 @Component({
 	selector: 'nav-bar',
-	imports: [CommonModule, RouterLink, RouterLinkActive],
+	imports: [CommonModule, RouterLink, RouterLinkActive, IdentityLogo],
 	templateUrl: './nav-bar.html',
 	styleUrl: './nav-bar.css',
 })
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { IdentityLogo } from '../../components/identity-logo/identity-logo';
 
 @Component({
 	selector: 'login-page',
 	templateUrl: './login.html',
 	styleUrl: './login.css',
+	imports: [IdentityLogo],
 })
 export class LoginPage {
 	private readonly auth = inject(AuthService);
