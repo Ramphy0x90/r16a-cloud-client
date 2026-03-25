586abf1ed9c14341bfdcc68bcda060ec743ccea7
ramphy <ramphy_an@outlook.com>
2026-03-25 20:21:31 +0100

    update: make theme selectable only in preferences

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,24 +1,30 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QuickSettings } from '../quick-settings/quick-settings';
 import { AuthService } from '../../services/auth.service';
 import { map, Observable } from 'rxjs';
 import { getUserInitials } from '../../utils/user-utils';
+import { Router } from '@angular/router';
+import { ROUTES } from '../../app.routes';
 
 @Component({
 	selector: 'app-header',
-	imports: [CommonModule, QuickSettings],
+	imports: [CommonModule],
 	templateUrl: './header.html',
 	styleUrl: './header.css',
 })
 export class Header {
 	private readonly authService = inject(AuthService);
+	private readonly router = inject(Router);
 
 	readonly isAuthenticated$: Observable<boolean> = this.authService.isAuthenticated$;
 	readonly userData$: Observable<Record<string, any>> = this.authService.userData$;
 	readonly userInitials$: Observable<string> = this.userData$.pipe(
 		map((user) => getUserInitials(user?.['name'] ?? '')),
 	);
+
+	toProfile(): void {
+		this.router.navigate([ROUTES.PROFILE]);
+	}
 
 	logout(): void {
 		this.authService.logout();
