10ef2ed16a6f1074f41867e45d6c248069a0aca2
ramphy <ramphy_an@outlook.com>
2026-05-25 10:37:23 +0200

    fix: blur hash in files

--- src/app/pages/files/grid-view/grid-view.ts
+++ src/app/pages/files/grid-view/grid-view.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IconFromExtensionPipe } from '../../../pipes/icon-from-extension-pipe';
 import { InViewportDirective } from '../../../directives/in-viewport.directive';
 import { FileViewBase } from '../file-view-base';
+import { blurhashToDataUrl } from '../../../utils/blurhash';
 
 @Component({
 	selector: 'grid-view',
@@ -10,4 +11,6 @@ import { FileViewBase } from '../file-view-base';
 	templateUrl: './grid-view.html',
 	styleUrl: './grid-view.css',
 })
-export class GridView extends FileViewBase {}
+export class GridView extends FileViewBase {
+	readonly blurhashToDataUrl = blurhashToDataUrl;
+}
